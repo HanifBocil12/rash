10a18642ee845ba2b017aed9b8acb1571fed6c67
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-05 08:45:36 +0700

    asas

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: streamlit run st.py --server.port=$PORT --server.address=0.0.0.0
-web: gunicorn server:app
--- pages/perlengkapan.py
+++ pages/perlengkapan.py
@@ -1,29 +1,21 @@
 import streamlit as st
 import os
-from components.utils import include_sidebar, load_css
-
-load_css()
-include_sidebar()
 
 st.title("⬇️ Download Perlengkapan")
 
-# Cari path absolut berdasarkan environment (Windows / Linux)
+# Path relatif (berdasarkan lokasi file ini)
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "static", "zip.rar")
-file_name = "web.rar"
 
-# Pastikan file ada
 if not os.path.exists(file_path):
     st.error(f"❌ File tidak ditemukan di: {file_path}")
 else:
-    # Baca file sebagai biner
     with open(file_path, "rb") as f:
         file_data = f.read()
 
-    # Tombol download
     st.download_button(
         label="💾 Download ZIP",
         data=file_data,
-        file_name=file_name,
+        file_name="web.rar",
         mime="application/x-rar-compressed"
     )
