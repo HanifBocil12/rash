dcc25e6ab01078dc5e8f5cf9a2ebbf704b1a079f
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-11 12:59:48 +0700

    sa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default defineConfig({
+  plugins: [react()],
+})
