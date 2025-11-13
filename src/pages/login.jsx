3801ceea5b77aa8b1a95b7ed5f90c8d23a809ed2
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-13 14:39:16 +0700

    Mo

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -186,7 +186,7 @@ const LiquidFlowLogin = () => {
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                  className="absolute right-3 top-[calc(50%+0.75rem)] -translate-y-1/2 flex items-center justify-center text-gray-500 hover:text-gray-700"
                 >
                   {showPassword ? (
                     <EyeOff className="h-5 w-5" />
