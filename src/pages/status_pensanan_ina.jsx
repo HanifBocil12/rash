f09cc79e31df8fab3d2f0da1cf99074fbd919b4e
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-13 08:28:37 +0700

    readonly

--- src/pages/status_pensanan_ina.jsx
+++ src/pages/status_pensanan_ina.jsx
@@ -225,6 +225,7 @@ export default function App() {
                     <label className="block text-sm font-medium text-gray-700 mb-2">Masukkan URL Railway API:</label>
                     <input
                     type="text"
+                    disabled
                     value={apiUrl}
                     onChange={(e) => setApiUrl(e.target.value)}
                     className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
