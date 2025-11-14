7fcd8ca92219ef67406049ccff613bfa2483ff18
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-14 11:18:45 +0700

    fa

--- src/db.js
+++ src/db.js
@@ -2,8 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // dari Railway
-    ssl: { rejectUnauthorized: false }
+  connectionString: "postgresql://postgres:aDPqkyMltnYJiJSwKHVoSltNnRIPUrqJ@postgres.railway.internal:5432/railway",
+  ssl: {
+    rejectUnauthorized: false, // penting untuk Railway
+  },
 });
 
 export default pool;
