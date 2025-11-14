d230c3fd124bdb9d2c8ae0f4ba35c670185b5c24
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-14 16:32:51 +0700

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ⬅️ Tambahkan ini
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>        {/* ⬅️ Bungkus App dengan BrowserRouter */}
-      <App />              {/* Komponen utama */}
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
