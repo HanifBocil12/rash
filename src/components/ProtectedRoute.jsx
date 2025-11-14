e46fbfef9428d122292ed9a61cceeaf2acc36e3c
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-14 15:33:11 +0700

    pft

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
 // src/components/ProtectedRoute.jsx
 import { Navigate, Outlet, useLocation, useParams } from "react-router-dom";
+import base64 from "base-64";
 
 export default function ProtectedRoute() {
   const location = useLocation();
@@ -11,8 +12,17 @@ export default function ProtectedRoute() {
     return <Navigate to="/" replace state={{ from: location }} />;
   }
 
+  // Decode ID dari URL untuk dicek
+  let decodedId = id;
+  try {
+    decodedId = base64.decode(id);
+  } catch (e) {
+    // Jika decode gagal, redirect ke home user
+    return <Navigate to={`/${user.id}/home`} replace />;
+  }
+
   // Cegah user A masuk URL user B
-  if (id && id !== String(user.id)) {
+  if (decodedId && decodedId !== String(user.id)) {
     return <Navigate to={`/${user.id}/home`} replace />;
   }
 
