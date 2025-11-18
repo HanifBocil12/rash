cd30b13eef1708a03401eb8ccae42903218d0093
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-18 11:58:38 +0700

    _

--- src/user.js
+++ src/user.js
@@ -14,7 +14,7 @@ async function seed() {
        VALUES ($1, $2, $3, $4, $5, $6)
        ON CONFLICT (id) DO NOTHING
        RETURNING id, nama, password, email, created_at, updated_at`,
-      [2, "admin2", hashedPassword, "admin2@example.com", now, now]
+      [2, "admin_2", hashedPassword, "admin2@example.com", now, now]
     );
 
     if (result.rowCount === 0) {
