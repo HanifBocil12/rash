08551cb62d87e66fe56ba2ef909284e698378c47
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-18 16:00:29 +0700

    togather

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -295,7 +295,7 @@ const LiquidFlowLogin = () => {
           </div>
         </div>
 
-        <div className="mt-6 sm:mt-8">
+        <div className="mt-[1.5vw] sm:mt-[2vw]">
           <footer className="text-center text-xs sm:text-[0.875vw]  text-gray-600">
             © 2025 Muhammad Hanif. Smkn 4 Tangerang.
           </footer>
