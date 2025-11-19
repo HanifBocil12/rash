be086da4d4cdc72b91f0f3e072cac2470f0a7c16
yujiro22445-ops <yujiro22445@gmail.com>
2025-11-19 07:56:03 +0700

    5

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -212,11 +212,7 @@ const LiquidFlowLogin = () => {
           Get Started
         </button>
 
-        <div className="  relative z-10
-                          flex flex-col items-center justify-center
-                          min-h-screen        /* HP potrait → center sempurna */
-                          md:min-h-0          /* Laptop/tablet → jangan pakai 100vh */
-                          py-[3vw] px-4 sm:px-6"
+        <div className="  relative z-10 flex flex-col items-center justify-center py-[3vw] px-4 sm:px-6"
         >
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-800 text-center leading-tight">
             <div>solusi hanif</div>
